f139472b061193908d71f5783e7ea54a5056d44e
wangjingbo <wangjingbo@innobuddy.com>
2016-12-14 19:27:42 +0800

    爬虫cnode 实例

--- entry/test.js
+++ entry/test.js
@@ -1,5 +1,14 @@
-'use strtic'
+'use strtic';
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+var targetUrl = 'https://cnodejs.org/';
 module.exports.hello = function *() {
-  console.log(this.request.query)
+  superagent.get(targetUrl)
+  .end(function (err, res) {
+    var $ = cheerio.load(res.text);
+    $('#topic_list .topic_title').each(function (idx, element) {
+      console.log($(element).attr('title'))
+    });
+  });
   this.body = this.request.body;
 }
